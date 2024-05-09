830fba22d746524b77d43d959e55c6eed1e4f44f
Krynnlord <rmiller.79@icloud.com>
2024-05-09 15:32:21 -0500

    changed the intro text to HP quote

--- TheOblivionCycle/src/intro.cpp
+++ TheOblivionCycle/src/intro.cpp
@@ -12,25 +12,27 @@ void intro()
 	Sleep(4000);
 
 	system("cls");
+
+	string storytext = "\n\n     The most merciful thing in the world, I think, is the inability \n";
+	typer(storytext, 30);
+	storytext = "     of the human mind to correlate all its contents.\n\n";
+	typer(storytext, 30);
+	Sleep(1000);
+	storytext = "     We live on a placid island of ignorance in the midst of black seas\n";
+	typer(storytext, 30);
+	storytext = "     of the infinity, and it was not meant that we should voyage far.\n\n";
+	typer(storytext, 30);
+	Sleep(1000);
+	storytext = "                                                     ~ H.P. Lovercraft";
+	typer(storytext, 30);
+	Sleep(4000);
+	
+	system("cls");
 	print_title();
 	//string titletext = "		          Part I: The Shimmering Gate";
 	//typer(titletext, 100);
 	Sleep(4000);
 
-	string storytext = "\n\n     Our story begins in a kingdom just south of the mighty river Hyatin,\n";
-	typer(storytext, 30);
-	storytext = "     known for its production of a special mineral named Shieya.\n\n";
-	typer(storytext, 30);
-	Sleep(1000);
-	storytext = "     This mineral has an unique property that many nations seek, and is\n";
-	typer(storytext, 30);
-	storytext = "     coveted beyond measure to the point where many are willing to kill for it.\n\n";
-	typer(storytext, 30);
-	Sleep(1000);
-	storytext = "     In these trying times a hero emerges....";
-	typer(storytext, 30);
-	Sleep(4000);
-	
 	ShowConsoleCursor(true);
 	return;
 }
