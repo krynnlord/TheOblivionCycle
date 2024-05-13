a3a97146f8d60ff04becd62468065f5ead7d6f90
Krynnlord <rmiller.79@icloud.com>
2024-05-13 14:45:24 -0500

    Added option to change armor and weapons in ESC menu

--- TheOblivionCycle/src/inventory.cpp
+++ TheOblivionCycle/src/inventory.cpp
@@ -52,69 +52,125 @@ void armor_checker(armor temp_armor)
 }
 void trinkets()
 {
+    int i = 0;
+    while (i == 0)
+    {
+
         print_trinkets();
         cout << endl;
         cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
-        if (rabbit_foot.have == 1) { trinket_checker(rabbit_foot); } else { ; }
-        if (dragon_talon.have == 1) { trinket_checker(dragon_talon); } else { ; }
-        if (azure_neclace.have == 1) { trinket_checker(azure_neclace); } else { ; }
-        if (witch_effegy.have == 1) { trinket_checker(witch_effegy); } else { ; }
-        if (vial_blood.have == 1) { trinket_checker(vial_blood); } else { ; }
-        if (frozen_tear.have == 1) { trinket_checker(frozen_tear); }  else { ; }
+        if (rabbit_foot.have == 1) { trinket_checker(rabbit_foot); }
+        else { ; }
+        if (dragon_talon.have == 1) { trinket_checker(dragon_talon); }
+        else { ; }
+        if (azure_neclace.have == 1) { trinket_checker(azure_neclace); }
+        else { ; }
+        if (witch_effegy.have == 1) { trinket_checker(witch_effegy); }
+        else { ; }
+        if (vial_blood.have == 1) { trinket_checker(vial_blood); }
+        else { ; }
+        if (frozen_tear.have == 1) { trinket_checker(frozen_tear); }
+        else { ; }
         cout << endl;
         cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
-        cout << " Press any key to return" << endl;
+        cout << " Press [ESC] to return or [R] to change " << endl;
         cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
 
         string ansr;
         ansr = _getch();
-
+        if (ansr == "r" or ansr == "R")
+        {
+            cout << "Coming Soon!"; Sleep(2000);
+        }
+        if (ansr == "0" or ansr == "\033")
+        {
+            i = 1;
+        }
+    
+    }
 }
 
 void weapons_display()
 {
-    print_weapons();
-    cout << endl;
-    cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
-    if (hands.have == 1) { weapon_checker(hands);  } else { ; }
-    if (dagger.have == 1) { weapon_checker(dagger); } else { ; }
-    if (club.have == 1) { weapon_checker(club); } else { ; }
-    if (handaxe.have == 1) { weapon_checker(handaxe); } else { ; }
-    if (mace.have == 1) { weapon_checker(mace); } else { ; }
-    if (shortsword.have == 1) { weapon_checker(shortsword); } else { ; }
-    if (lighthammer.have == 1) { weapon_checker(lighthammer); } else { ; }
-    if (warhammer.have == 1) { weapon_checker(warhammer); } else { ; }
-    if (longsword.have == 1) { weapon_checker(longsword); } else { ; }
-    if (greatsword.have == 1) { weapon_checker(greatsword); } else { ; }
-    if (maul.have == 1) { weapon_checker(maul); } else { ; }
-    cout << endl;
-    cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
-    cout << " Press any key to return" << endl;
-    cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
+    int i = 0;
+    while (i == 0)
+    {
 
-    string ans;
-    ans = _getch();
+        print_weapons();
+        cout << endl;
+        cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
+        if (hands.have == 1) { weapon_checker(hands); }
+        else { ; }
+        if (dagger.have == 1) { weapon_checker(dagger); }
+        else { ; }
+        if (club.have == 1) { weapon_checker(club); }
+        else { ; }
+        if (handaxe.have == 1) { weapon_checker(handaxe); }
+        else { ; }
+        if (mace.have == 1) { weapon_checker(mace); }
+        else { ; }
+        if (shortsword.have == 1) { weapon_checker(shortsword); }
+        else { ; }
+        if (lighthammer.have == 1) { weapon_checker(lighthammer); }
+        else { ; }
+        if (warhammer.have == 1) { weapon_checker(warhammer); }
+        else { ; }
+        if (longsword.have == 1) { weapon_checker(longsword); }
+        else { ; }
+        if (greatsword.have == 1) { weapon_checker(greatsword); }
+        else { ; }
+        if (maul.have == 1) { weapon_checker(maul); }
+        else { ; }
+        cout << endl;
+        cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
+        cout << " Press [ESC] to return or [R] to change " << endl;
+        cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
 
+        string ansr;
+        ansr = _getch();
+        if (ansr == "r" or ansr == "R")
+        {
+            change_weapon();
+        }
+        if (ansr == "0" or ansr == "\033")
+        {
+            i = 1;
+        }
+    }
 }
 
 void armor_display()
 {
-    print_armor();
-    cout << endl;
-    cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
-    if (tunic.have == 1) { armor_checker(tunic); } else { ; }
-    if (cloth_armor.have == 1) { armor_checker(cloth_armor); } else { ; }
-    if (leather_armor.have == 1) { armor_checker(leather_armor); } else { ; }
-    if (chain_armor.have == 1) { armor_checker(chain_armor); } else { ; }
-    if (scale_armor.have == 1) { armor_checker(scale_armor); } else { ; }
-    if (splint_armor.have == 1) { armor_checker(splint_armor); } else { ; }
-    if (plate_armor.have == 1) { armor_checker(plate_armor); } else { ; }
-    cout << endl;
-    cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
-    cout << " Press any key to return" << endl;
-    cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
+    int i = 0;
+    while (i == 0)
+    {
+    
+        print_armor();
+        cout << endl;
+        cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
+        if (tunic.have == 1) { armor_checker(tunic); } else { ; }
+        if (cloth_armor.have == 1) { armor_checker(cloth_armor); } else { ; }
+        if (leather_armor.have == 1) { armor_checker(leather_armor); } else { ; }
+        if (chain_armor.have == 1) { armor_checker(chain_armor); } else { ; }
+        if (scale_armor.have == 1) { armor_checker(scale_armor); } else { ; }
+        if (splint_armor.have == 1) { armor_checker(splint_armor); } else { ; }
+        if (plate_armor.have == 1) { armor_checker(plate_armor); } else { ; }
+        cout << endl;
+        cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
+        cout << " Press [ESC] to return or [R] to change " << endl;
+        cout << "---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---~~~---" << endl;
 
-    string ans;
-    ans = _getch();
+        string ansr;
+        ansr = _getch();
+        if (ansr == "r" or ansr == "R")
+        {
+            change_armor();
+        }
+        if (ansr == "0" or ansr == "\033")
+        {
+            i = 1;
+        }
+
+    }
 
 }
