ff2e695bf635924867dc3a4594af3177e9c8b0b0
Krynnlord <rmiller.79@icloud.com>
2024-06-07 11:45:02 -0500

    Beautified the Monster Display screen

--- TheOblivionCycle/src/monsterDisplay.cpp
+++ TheOblivionCycle/src/monsterDisplay.cpp
@@ -4,19 +4,19 @@ void monster_display(monster enemy)
 {
     if (enemy.name == zombie.name) 
     {
-        enemy.desc = "Listless, humanoid corpses, reanimated as guardians by powerful clerics or wizards.";
+        enemy.desc = "Listless, humanoid corpses, reanimated as guardians by\n"
+                     "powerful clerics or wizards.";
     }
     if (enemy.name == skeleton.name)
     {
-        enemy.desc = "Skeletal remains of humanoids, reanimated as guardians by powerful magic-users or clerics. Often encountered in cemeteries, crypts, or other forlorn places.";
-    }
-    if (enemy.name == skeleton.name)
-    {
-        enemy.desc = "Skeletal remains of humanoids, reanimated as guardians by powerful magic-users or clerics. Often encountered in cemeteries, crypts, or other forlorn places.";
+        enemy.desc = "Skeletal remains of humanoids, reanimated as guardians\n"
+                     "by powerful magic-users or clerics. Often encountered \n"
+                     "in cemeteries, crypts, or other forlorn places.";
     }
     if (enemy.name == giant_boar.name)
     {
-        enemy.desc = "Omnivorous wild boars that dwell primarily in forests. Can be irascible and dangerous, if disturbed.";
+        enemy.desc = "Omnivorous wild boars that dwell primarily in forests.\n"
+                     "Can be irascible and dangerous, if disturbed.";
     }
     if (enemy.name == giant_bat.name)
     {
@@ -28,23 +28,29 @@ void monster_display(monster enemy)
     }
     if (enemy.name == goblin.name)
     {
-        enemy.desc = "Small, grotesque humanoids with pallid, earth-coloured skin and glowing, red eyes. Dwell underground.";
+        enemy.desc = "Small, grotesque humanoids with pallid, earth-coloured\n"
+                     "skin and glowing, red eyes. Dwell underground.";
     }
     if (enemy.name == goblin_warrior.name)
     {
-        enemy.desc = "Small, grotesque humanoids with pallid, earth-coloured skin and glowing, red eyes. Dwell underground.";
+        enemy.desc = "Small, grotesque humanoids with pallid, earth-coloured\n"
+                     "skin and glowing, red eyes. Dwell underground.";
     }
     
     system("cls");
-    int bar1; for (bar1 = 0;bar1 < 55;bar1++) { cout << "-"; } cout << endl;
-    cout << left << setw(20); cout << enemy.name;
-    cout << "Level: " << left << setw(2); cout << enemy.level;
-    cout << "  HP:" << right << setw(3); cout << enemy.hp << "/" << left << setw(3); cout << enemy.hp_max;
-    cout << "  Damage: " << left << setw(3); cout << enemy.damage;
+    Color(10); cout << "Monster Information" << endl; Color(7);
+    int bar1; for (bar1 = 0;bar1 < 55;bar1++) { cout << "-"; };
     cout << endl;
-    int bar; for (bar = 0;bar < 55;bar++) { cout << "-"; };
-    cout << endl << "Description:" << endl;
-    cout << enemy.desc << endl << endl;
-    cout << endl << endl; system("pause");
+    Color(14); cout << "Name: ";Color(7); Color(7); cout << enemy.name << endl;
+    Color(14); cout << "Level: ";Color(7); cout << enemy.level << endl;
+    Color(14); cout << "HP: ";Color(7); cout << enemy.hp << "/" << left << setw(3); cout << enemy.hp_max << endl;
+    Color(14); cout << "Damage: ";Color(7); cout << left << setw(3); cout << enemy.damage;
+    cout << endl;
+    for (bar1 = 0;bar1 < 55;bar1++) { cout << "-"; };
+    Color(14); cout << endl << "Description:\n"; Color(7);
+    cout << enemy.desc << endl;
+    for (bar1 = 0;bar1 < 55;bar1++) { cout << "-"; } cout << endl;
+    string ans;
+    ans = _getch();
     return;
 }
